be654f5c805aa5e7b8ccdd5033da195ff38fe5a3
Sangeeth Promod <92209765+sangeethpromod@users.noreply.github.com>
2025-10-22 16:07:58 +0530

    Delete .mastra directory

--- .mastra/output/tools.mjs
+++
@@ -1,6 +0,0 @@
-import * as tool0 from './tools/9f0c79b3-c83a-4e5a-a31e-2e75595a9f59.mjs';
-import * as tool1 from './tools/7a6ed989-3d99-463f-a2d8-3b3623bc8e02.mjs';
-import * as tool2 from './tools/118e21be-34b0-412a-a452-05d760390d74.mjs';
-import * as tool3 from './tools/2754791b-91e9-4394-881c-9188bc0dbffc.mjs';
-        
-                export const tools = [tool0, tool1, tool2, tool3]
